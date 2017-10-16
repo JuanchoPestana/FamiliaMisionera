314bb08bf2d781d721a7319d271ee7e4ac7b7e58
Juan Alfredo Pestana Viso <juancho.pestana@gmail.com>
2017-10-16 08:02:13 -0500

    First commit.

---
+++ FMisionera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FMisionera
+//
+//  Created by Juancho Pestana on 3/27/17.
+//  Copyright © 2017 DPSoftware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
